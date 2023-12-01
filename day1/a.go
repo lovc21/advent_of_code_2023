4c280a8d7a7f770830eee609399c2552e4ae468c
lovc21 <jakob.dekleva@gmail.com>
2023-12-01 23:23:44 +0100

    Feat: add second prat for day one

--- day1/a.go
+++ day1/a.go
@@ -2,57 +2,59 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 )
 
-func task() {
-
-	var str string = `1abc2
-	pqr3stu8vwx
-	a1b2c3d4e5f
-	treb7uchet`
-
-	// split stri in to lines
-	lines := strings.Split(str, "\n")
-
-	//check if number is in line
-	newNumberArr := []int{}
-	for _, line := range lines {
-		firstNum, lastNum := -1, -1
-		for _, ch := range line {
-
-			if s, err := strconv.Atoi(string(ch)); err == nil {
-				if firstNum == -1 {
-					firstNum = s
-				}
-				lastNum = s
-			}
-		}
-		if firstNum != -1 {
-			newNumberArr = append(newNumberArr, firstNum)
-		}
-		if lastNum != -1 {
-			newNumberArr = append(newNumberArr, lastNum)
-		}
+func wordToDigit(word string) (int, bool) {
+	numberMap := map[string]int{
+		"one": 1, "two": 2, "three": 3, "four": 4,
+		"five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
 	}
-	finalNumberArr := []int{}
+	value, exists := numberMap[word]
+	return value, exists
+}
 
-	for i := 0; i < len(newNumberArr); i = i + 2 {
-		if i+1 < len(newNumberArr) {
-			combine, _ := strconv.Atoi(strconv.Itoa(newNumberArr[i]) + strconv.Itoa(newNumberArr[i+1]))
-			finalNumberArr = append(finalNumberArr, combine)
-		}
+func extractDigits(line string) (int, int, bool) {
+	words := strings.FieldsFunc(line, func(r rune) bool {
+		return !('a' <= r && r <= 'z')
+	})
+
+	if len(words) == 0 {
+		return 0, 0, false
 	}
 
-	final := 0
-
-	for i := 0; i < len(finalNumberArr); i = i + 1 {
-		final = final + finalNumberArr[i]
+	firstDigit, ok := wordToDigit(words[0])
+	if !ok {
+		return 0, 0, false
 	}
 
-	fmt.Println(final)
-	//fmt.Println(finalNumberArr)
-	//fmt.Println(newNumberArr)
-	//fmt.Println(lines)
+	lastDigit, ok := wordToDigit(words[len(words)-1])
+	if !ok {
+		return 0, 0, false
+	}
+
+	return firstDigit, lastDigit, true
+}
+
+func main() {
+	inputLines := []string{
+		"two1nine",
+		"eightwothree",
+		"abcone2threexyz",
+		"xtwone3four",
+		"4nineeightseven2",
+		"zoneight234",
+		"7pqrstsixteen",
+	}
+
+	totalSum := 0
+	for _, line := range inputLines {
+		firstDigit, lastDigit, found := extractDigits(line)
+		if found {
+			calibrationValue := firstDigit*10 + lastDigit
+			totalSum += calibrationValue
+		}
+	}
+
+	fmt.Println("Total Sum of Calibration Values:", totalSum)
 }
